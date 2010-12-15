dbe6dfaf6f229a852acdaad985c87eb4723e4988
Matthew Toseland <toad@amphibian.dyndns.org>
2010-12-15 20:20:52 +0000

    Fix for 1311.

--- src/plugins/ThawIndexBrowser/ThawIndexBrowser.java
+++ src/plugins/ThawIndexBrowser/ThawIndexBrowser.java
@@ -348,7 +348,7 @@ public class ThawIndexBrowser implements FredPlugin, FredPluginThreadless, FredP
 
 				public boolean run(ObjectContainer container, ClientContext context) {
 					try {
-						fcp.makePersistentGlobalRequest(fetchURI, false, null, "forever", "disk", container);
+						fcp.makePersistentGlobalRequest(fetchURI, false, null, "forever", "disk", false, container);
 					} catch (NotAllowedException e) {
 						Logger.normal(this, "Failed to make persistent request: "+e, e);
 					} catch (IOException e) {
